8e70190c8d26664fd5898458af725bc1116c3e29
bharath <bharath.raju.1000@gmail.com>
2026-03-09 15:37:47 +0530

    project 3 : routers , authentication , JWT

--- 3_todo/main.py
+++ 3_todo/main.py
@@ -1,36 +1,8 @@
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI
 import models
-from database import engine,SessionLocal
-from sqlalchemy.orm import Session
-from typing import Annotated
-from models import Todos
+from database import engine
+from routers import auth,todos
 app = FastAPI()
-
+app.include_router(auth.router)
+app.include_router(todos.router)
 models.Base.metadata.create_all(bind=engine)
-
-def get_db():
-    db=SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@app.get('/')
-def get_all(db : Annotated[Session,Depends(get_db)] ):
-    return db.query(Todos).all()
-
-# app.pst()
-# func(todorequest)
-# 	todo_model=TODOS(**todorequest.dict())
-	
-# 	db.add(new_model)
-# 	db.commit()
-
-# Put requesT:
-# While updating we need to keep total naming, because, it might think we are adding new one and ID is incremented.
-# Filter from db and 
-# Give the value to that model by assigning request values.
-
-# Delete request:
-# filter().delete() #deletes that model.
--- 3_todo/models.py
+++ 3_todo/models.py
@@ -1,8 +1,10 @@
 from database import Base
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 
+
 class Users(Base):
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True)
     username = Column(String, unique=True)
@@ -12,8 +14,10 @@ class Users(Base):
     is_active = Column(Boolean, default=True)
     role = Column(String)
 
-class Todos(Base): #inherited Base and added table schema
+
+class Todos(Base):
     __tablename__ = 'todos'
+
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     description = Column(String)
